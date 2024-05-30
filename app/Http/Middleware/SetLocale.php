1ef0c244be4d405050a553c69443b583970a895f
Antoine Auger <Ardyn142@gmail.com>
2024-05-30 00:51:45 -0400

    Initial commit

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\URL;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->segment(1) != null && in_array($request->segment(1), config('app.available_locales'))) {
+            app()->setLocale($request->segment(1));
+            URL::defaults(['locale' => $request->segment(1)]);
+            return $next($request);
+        } else {
+            return redirect(app()->getLocale());
+        }
+        return $next($request);
+    }
+}
